88397b74586e1a44a99b4bf042615e73c4d7ecca
zhixc <eg79@qq.com>
2023-10-11 20:55:31 +0800

    修复哔哩兔

--- app/src/main/java/com/github/catvod/demo/TestBilituys.java
+++ app/src/main/java/com/github/catvod/demo/TestBilituys.java
@@ -1,36 +1,63 @@
 package com.github.catvod.demo;
 
+import android.content.Context;
 import com.github.catvod.spider.Bilituys;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class TestBilituys {
-    public static void main(String[] args) {
-        Bilituys bilituys = new Bilituys();
+    Bilituys bilituys;
+
+    @Before
+    public void init() throws Exception {
+        bilituys = new Bilituys();
+        bilituys.init(new Context(), "");
+    }
+
+    @Test
+    public void homeContent() throws Exception {
         // 首页内容测试
-//        String s = bilituys.homeContent(true);
-//        System.out.println(s);
+        System.out.println(bilituys.homeContent(true));
+    }
 
+    @Test
+    public void homeVideoContent() throws Exception {
+    }
+
+    @Test
+    public void categoryContent() throws Exception {
         // 分类内容测试
-//        HashMap<String, String> map = new HashMap<>();
-//        map.put("year", "2023");
-//        String s = bilituys.categoryContent("1", "1", true, map);
-//        System.out.println(s);
+        HashMap<String, String> extend = new HashMap<>();
+        extend.put("year", "2023");
+//        System.out.println(bilituys.categoryContent("1", "1", true, extend));
+        System.out.println(bilituys.categoryContent("1", "2", true, extend));
+    }
 
+    @Test
+    public void detailContent() throws Exception {
         // 详情内容测试
-//        List<String> list = new ArrayList<>();
-//        list.add("https://www.bilituys.com/bilidetail/85267.html");
-//        String s = bilituys.detailContent(list);
-//        System.out.println(s);
+        List<String> ids = new ArrayList<>();
+        ids.add("/bilidetail/168012.html");
+//        ids.add("/bilidetail/53926.html");
+        System.out.println(bilituys.detailContent(ids));
+    }
 
+    @Test
+    public void searchContent() throws Exception {
         // 搜索内容测试
-//        String s = bilituys.searchContent("我", true);
-//        System.out.println(s);
+//        System.out.println(bilituys.searchContent("我", true));
+        System.out.println(bilituys.searchContent("我", true, "2"));
+    }
 
+    @Test
+    public void playerContent() throws Exception {
         // 播放内容测试
-        String s = bilituys.playerContent("", "https://www.bilituys.com/biliplay/85267-1-1.html", new ArrayList<>());
-        System.out.println(s);
+        List<String> vipFlags = new ArrayList<>();
+//        System.out.println(bilituys.playerContent("哔哩¹13", "/biliplay/53926-1-1.html", vipFlags));
+        System.out.println(bilituys.playerContent("大雄13", "/biliplay/53926-2-1.html", vipFlags));
     }
 }
